4d3bcf1d25207b39b27b0697961eb7dd9d511a12
TR-SLimey <37966924+TR-SLimey@users.noreply.github.com>
2021-02-07 18:04:15 +0000

    Initial code - Basic command flags parsing (only config location for now) - Reading and parsing config

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/TR-SLimey/DroneExternalConfig
 
 go 1.15
+
+require (
+	github.com/galdor/go-cmdline v1.1.1
+	github.com/vaughan0/go-ini v0.0.0-20130923145212-a98ad7ee00ec
+)
--- main.go
+++ main.go
@@ -1,5 +1,37 @@
 package main
 
+import (
+	"log"
+	"os"
+
+	cmdline "github.com/galdor/go-cmdline"
+	ini "github.com/vaughan0/go-ini"
+)
+
 func main() {
-	println("Hello World!")
+	// Handle command line options/arguments (-h/--help is implicit)
+	cl := cmdline.New()
+	cl.AddOption("c", "conf", "file", "path to the config file")
+	cl.SetOptionDefault("conf", "/etc/DroneExternalConfig/config.ini")
+	cl.Parse(os.Args)
+
+	// Attempt to parse config file
+	configPath := cl.OptionValue("conf")
+	config, err := ini.LoadFile(configPath)
+	if err != nil {
+		log.Fatalf("I couldn't parse the config file because of an error: %s", err.Error())
+	}
+
+	// Get the important sections of the config file
+	serverConfig, serverConfigExists := config["server"]
+	if !serverConfigExists {
+		log.Fatalf("The config file seems to be missing the [server] section. Please add it and re-start DroneExternalConfig.")
+	}
+	mappingsConfig, mappingsConfigExists := config["config-map"]
+	if !mappingsConfigExists {
+		log.Fatalf("The config file seems to be missing the [config-map] section. Please add it and re-start DroneExternalConfig.")
+	}
+
+	// Start the server
+	// TODO
 }
